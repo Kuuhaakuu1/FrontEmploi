38d3b7cd768337d2d78a3ec3d4656594cde7fdfd
Omar Temsamani <omartemsamani22@gmail.com>
2024-01-14 15:47:12 +0100

    Add activity to student

--- src/app/activities/activities.component.ts
+++ src/app/activities/activities.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { Activity } from '../entitiy/activity';
 import { StudentService } from '../service/student.service';
 import { AdminService } from '../service/admin.service';
+import { Table } from 'primeng/table';
 
 @Component({
   selector: 'app-activities',
   templateUrl: './activities.component.html',
-  styleUrl: './activities.component.scss'
+  styleUrl: './activities.component.scss',
 })
 export class ActivitiesComponent {
   activities!: Activity[];
@@ -16,12 +17,24 @@ export class ActivitiesComponent {
     private httpAdminService: AdminService
   ) {}
 
-
-  
   ngOnInit() {
     this.httpAdminService.getAllActivities().subscribe((data) => {
       this.activities = data;
       console.log(this.activities);
     });
+  }
+  clear(_t10: Table) {
+    throw new Error('Method not implemented.');
+  }
+  addActivityToStudent(id: number) {
+    this.httpservice
+      .addActivityToCalendar(
+        parseInt(localStorage.getItem('currentUser') ?? ''),
+        id
+      )
+      .subscribe((data) => {
+        this.activities.push(data);
+      });
+    window.location.reload();
   }
 }
